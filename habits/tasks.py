ddc2a8f809336db5f51448b7c33484a7a9271b80
ValentinaGuryanova <gvalushka@mail.ru>
2024-01-04 01:10:05 +0300

    Проект курсовой (устранение замечаний)

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,29 +1,26 @@
+from datetime import datetime
+import os
+
 import requests
 from celery import shared_task
-from django.conf import settings
 
-import habits
-from config.settings import TELEGRAM_CHAT_ID, TELEGRAM_BOT_API_KEY
 from habits.models import Habit
-
-bot_token = TELEGRAM_BOT_API_KEY
-telegram_id = TELEGRAM_CHAT_ID
-get_id_url = f'https://api.telegram.org/bot{bot_token}/getUpdates'
-send_message_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
 
 
 @shared_task
-def send_message_to_bot(habit_id):
+def send_message_to_bot() -> None:
     """ функция отправки сообщения в телеграм-бот
     chat_id: id чата
     message: передаваемое сообщение
     """
-    habit = Habit.objects.get(id=habit_id)
+    token = os.getenv('TELEGRAM_BOT_API_KEY')
+    obj = Habit.objects.all()
 
-    requests.post(
-        url=f'{settings.TELEGRAM_URL}{settings.TELEGRAM_BOT_API_KEY}/sendMessage',
-        data={
-            'chat_id': habit.user.telegram_id,
-            'text': habits.services.create_message
-        }
-    )
+    for item in obj:
+        if item.time == datetime.now().time():
+            message = f'Привет {item.user}! Время {item.time}. Пора идти в {item.place} и сделать {item.action}. ' \
+                      f'Это займет {item.duration} минут!'
+            send_message_url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={item.user.telegram_id}&text={message}"
+            requests.get(send_message_url)
+        else:
+            print("Еще не пришло время выполнять привычку!")
