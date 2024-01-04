e93d64efe9c32adb8fa1a928f8bf80632780d80d
ValentinaGuryanova <gvalushka@mail.ru>
2024-01-04 22:19:05 +0300

    Проект курсовой (доработка)

--- habits/services.py
+++ habits/services.py
@@ -1,6 +1,74 @@
-from django_celery_beat.models import PeriodicTask, CrontabSchedule
+from django_celery_beat.models import CrontabSchedule
 
 from config import settings
+from django.http import HttpResponse
+from django_celery_beat.models import PeriodicTask
+
+from datetime import datetime
+
+import pytz
+
+from habits.models import Habit
+from habits.tasks import telegram_id, send_message_to_bot
+
+
+def check_habits_daily():
+    """ Проверка ежедневных привычек на выполнение """
+
+    date_time_now = datetime.now()
+    moscow_timezone = pytz.timezone('Europe/Moscow')
+    date_now = date_time_now.astimezone(moscow_timezone)
+    time_now = date_now.time()
+
+    habits = Habit.objects.filter(time__hour=time_now.hour, time__minute=time_now.minute,
+                                  period='ежедневно', habit_is_good=False)
+
+    for habit in habits:
+        create_message(habit.id)
+
+
+def check_habits_weekly():
+    """ Проверка еженедельных привычек на выполнение """
+
+    date_time_now = datetime.now()
+    moscow_timezone = pytz.timezone('Europe/Moscow')
+    date_now = date_time_now.astimezone(moscow_timezone)
+    time_now = date_now.time()
+
+    habits = Habit.objects.filter(time__hour=time_now.hour, time__minute=time_now.minute,
+                                  period='еженедельно', habit_is_good=False)
+
+    for habit in habits:
+        create_message(habit.id)
+
+
+def create_message(habit_id):
+    """ Функция создания сообщения для отправки в телеграм-бот """
+
+    habit = Habit.objects.get(id=habit_id)
+
+    user = habit.user
+    time = habit.time
+    action = habit.action
+    place = habit.place
+    duration = round(habit.duration.total_seconds() / 60)
+
+    message = f'Привет {user}! Время {time}. Пора идти в {place} и сделать {action}. ' \
+              f'Это займет {duration} минут!'
+
+    response = send_message_to_bot(telegram_id, message)
+    if habit.connected_habit:
+        habit_is_good_id = habit.connected_habit.id
+        habit_is_good = Habit.objects.get(id=habit_is_good_id)
+        nice_time = round(habit_is_good.duration.total_seconds() / 60)
+        message = (f'Молодец! Ты выполнил {action}, за это тебе подарок {habit_is_good.action} '
+                   f'в течение {nice_time} минут')
+
+        time.sleep(10)
+        nice_response = send_message_to_bot(telegram_id, message)
+        return HttpResponse(nice_response)
+
+    return HttpResponse(response)
 
 
 def create_reminder(habit):
--- habits/tasks.py
+++ habits/tasks.py
@@ -1,26 +1,29 @@
-from datetime import datetime
-import os
-
 import requests
 from celery import shared_task
+from django.conf import settings
 
+from config.settings import TELEGRAM_CHAT_ID, TELEGRAM_BOT_API_KEY
 from habits.models import Habit
+
+bot_token = TELEGRAM_BOT_API_KEY
+telegram_id = TELEGRAM_CHAT_ID
+get_id_url = f'https://api.telegram.org/bot{bot_token}/getUpdates'
+send_message_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
 
 
 @shared_task
-def send_message_to_bot() -> None:
+def send_message_to_bot(habit_id):
     """ функция отправки сообщения в телеграм-бот
     chat_id: id чата
     message: передаваемое сообщение
     """
-    token = os.getenv('TELEGRAM_BOT_API_KEY')
-    obj = Habit.objects.all()
-
-    for item in obj:
-        if item.time == datetime.now().time():
-            message = f'Привет {item.user}! Время {item.time}. Пора идти в {item.place} и сделать {item.action}. ' \
-                      f'Это займет {item.duration} минут!'
-            send_message_url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={item.user.telegram_id}&text={message}"
-            requests.get(send_message_url)
-        else:
-            print("Еще не пришло время выполнять привычку!")
+    habit = Habit.objects.get(id=habit_id)
+    for item in habit:
+        requests.post(
+            url=f'{settings.TELEGRAM_URL}{settings.TELEGRAM_BOT_API_KEY}/sendMessage',
+            data={
+                'chat_id': item.user.telegram_id,
+                'text': f'Привет {item.user}! Время {item.time}. Пора идти в {item.place} и сделать {item.action}. ' \
+                        f'Это займет {item.duration} минут!'
+            }
+        )
